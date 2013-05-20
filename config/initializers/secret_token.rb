4c5866c123e9f0b1caa7b0d1f730b5e0ded01fe8
anromeo <anthonykzullo@gmail.com>
2013-05-20 14:34:28 -0700

    Added Readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollaborativeQAndA::Application.config.secret_token = '14cc8c7f6f93e250ef6a87f66403494deeb83512f619ffa19df686bfddeea8444d62370f26c0171f82ab58f3e0b9f21989a54db8c21428631897487348829238'
